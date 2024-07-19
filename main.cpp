817d07f101dfef0e4416aff5fb9cc1d90c2c64f1
yang <19980103ly@gmail.com>
2024-07-19 23:21:52 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include "qrencode.h"
+
+int main()
+{
+	QRcode* pQRC = pQRC = QRcode_encodeString("https://www.baidu.com", 0, QRecLevel::QR_ECLEVEL_H, QR_MODE_8, 1);
+	std::cout << "Hello World!\n";
+}
